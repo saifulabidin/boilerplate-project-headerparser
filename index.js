9ef9b7b2ee6972331b5bbf707d7e38fd23385e65
Saiful Abidin <55196633+saifulabidin@users.noreply.github.com>
2025-04-12 01:50:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -20,9 +20,12 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+  res.json({ language: req.headers["accept-language"],
+            software: req.headers["user-agent"],
+            ipaddress: req.headers["x-forwarded-for"] });
 });
+
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
